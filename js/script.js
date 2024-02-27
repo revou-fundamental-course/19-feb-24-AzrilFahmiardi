6cf8d33a9f2fceb457fb88cfdab8bc6bf82b5baf
AzrilFahmiardi <anakijummira@gmail.com>
2024-02-27 21:47:13 +0700

    menambahkan isi dari fitur message us

--- js/script.js
+++ js/script.js
@@ -3,4 +3,29 @@ function replaceName() {
   document.getElementById("visitor").innerHTML = visitor;
 }
 
+function setSenderUI(name, birthDate, gender, messages) {
+  document.getElementById("output-nama").innerHTML = name;
+  document.getElementById("output-tanggal-lahir").innerHTML = birthDate;
+  document.getElementById("output-gender").innerHTML = gender;
+  document.getElementById("output-pesan").innerHTML = messages;
+}
+
+function validateForm(event) {
+  const name = document.forms["message-form"]["fullname"].value;
+  const birthDate = document.forms["message-form"]["birth-date"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const messages = document.forms["message-form"]["messages"].value;
+
+  if (name == "" || birthDate == "" || gender == "" || messages == "") {
+    alert("Data tidak boleh kosong");
+    event.preventDefault();
+  }
+
+  setSenderUI(name, birthDate, gender, messages);
+  event.preventDefault();
+}
+
+let submit = document.getElementById("submit");
+
 addEventListener("load", replaceName);
+submit.addEventListener("click", validateForm);
