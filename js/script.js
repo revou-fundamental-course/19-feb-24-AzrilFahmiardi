acb0915babbb122e0b06425b9373165f066ef4da
AzrilFahmiardi <anakijummira@gmail.com>
2024-02-28 20:01:52 +0700

    membuat slideshow dan mengganti footer

--- js/script.js
+++ js/script.js
@@ -26,6 +26,53 @@ function validateForm(event) {
 }
 
 let submit = document.getElementById("submit");
-
 addEventListener("load", replaceName);
+
 submit.addEventListener("click", validateForm);
+
+function showArrow() {
+  let arrow = document.querySelector(".slide-arrow");
+  arrow.style.visibility = "visible";
+}
+
+let slideSHow = document.querySelectorAll(".img-slide-show");
+
+slideSHow.forEach(function (slideSHow) {
+  slideSHow.addEventListener("mouseenter", showArrow);
+});
+
+let slideIndex = 0;
+showSlide();
+
+function plusSlide() {
+  slideIndex += 1;
+  showSlide();
+}
+
+function showSlide() {
+  let x = document.getElementsByClassName("img-slide-show");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  if (slideIndex >= x.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = x.length - 1;
+  }
+  x[slideIndex].style.display = "block";
+}
+
+let next = document.querySelector(".next");
+let previous = document.querySelector(".previous");
+
+next.addEventListener("click", function () {
+  slideIndex += 1;
+  showSlide();
+});
+previous.addEventListener("click", function () {
+  slideIndex -= 1;
+  showSlide();
+});
+
+setInterval(plusSlide, 2000);
